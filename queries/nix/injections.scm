6328de6ca12ed700e379e25145bc950fa72452fb
Rémi Labeyrie <calops@tocards.net>
2023-08-02 20:21:14 +0200

    feat: generic xdg.configFile support

--- queries/nix/injections.scm
+++ queries/nix/injections.scm
@@ -1,17 +1,23 @@
 ;; extends
 
-;; Generic
+; Generic
+(binding
+  attrpath: (_) @_path (#match? @_path "text$")
+  expression: (_
+    (string_fragment) @injection.content
+  )
+  (#hmts-path? @_path "xdg" "configFile" ".*" "text")
+  (#hmts-inject! @_path)
+) @combined
 
-
-;; Fish
+; Fish
 (binding
   attrpath: (_) @_path (#match? @_path "((interactive|login)S|s)hellInit$")
-  expression: [
-		(string_expression (string_fragment) @fish)
-		(indented_string_expression (string_fragment) @fish)
-	]
-  (#nix-path? @_path "programs" "fish")
-)
+  expression: (_ (string_fragment) @fish)
+	
+  ; I would prefer to have an accurate pattern here but lua regex are so awful, it's not possible
+  (#hmts-path? @_path "programs" "fish" ".*")
+) @combined
 
 (binding
   attrpath: (_) @_path (#match? @_path "functions$")
@@ -19,12 +25,14 @@
     (binding_set
       binding: (binding
         attrpath: (_)
-        expression: [
-          (string_expression (string_fragment) @fish)
-          (indented_string_expression (string_fragment) @fish)
-        ]
-      )
-    )
-  )
-  (#nix-path? @_path "programs" "fish")
-)
+        expression: (_ (string_fragment) @fish)
+  )))
+  (#hmts-path? @_path "programs" "fish" "functions")
+) @combined
+
+; Firefox
+(binding
+  attrpath: (_) @_path (#match? @_path "userChrome$")
+  expression: (_ (string_fragment) @css)
+  (#hmts-path? @_path "programs" "firefox" "profiles" ".*" "userChrome")
+) @combined
