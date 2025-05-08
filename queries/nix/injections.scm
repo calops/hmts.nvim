21dbae7d960a25e0ff5b1c2ea461c0f0ad56572b
Manuel Transfeld <auipga@users.noreply.github.com>
2025-05-08 22:46:31 +0200

    Fix zsh
    fixes #25

--- queries/nix/injections.scm
+++ queries/nix/injections.scm
@@ -75,7 +75,7 @@
 ; Zsh
 (binding
   attrpath: (_) @_path
-  (#hmts-path? @_path "programs" "bash" "(completionInit|envExtra|initExtra|initExtraBeforeCompInit|initExtraFirst|loginExtra|logoutExtra|profileExtra)$")
+  (#hmts-path? @_path "programs" "zsh" "(completionInit|envExtra|initExtra|initExtraBeforeCompInit|initExtraFirst|loginExtra|logoutExtra|profileExtra)$")
   expression: (_ (string_fragment) @injection.content)
   (#set! injection.language "bash")
   (#set! injection.combined)
