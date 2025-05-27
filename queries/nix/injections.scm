21b41207a89e5119072aa74947ac799d75d29536
Manuel Transfeld <auipga@users.noreply.github.com>
2025-05-27 17:48:21 +0200

    add oh-my-zsh
    programs.zsh.oh-my-zsh.extraConfig -> bash

--- queries/nix/injections.scm
+++ queries/nix/injections.scm
@@ -81,6 +81,15 @@
   (#set! injection.combined)
 )
 
+; oh-my-zsh
+(binding
+  attrpath: (_) @_path
+  (#hmts-path? @_path "programs" "zsh" "oh-my-zsh" "extraConfig$")
+  expression: (_ (string_fragment) @injection.content)
+  (#set! injection.language "bash")
+  (#set! injection.combined)
+)
+
 ; Firefox
 (binding
   attrpath: (_) @_path (#hmts-path? @_path "programs" "firefox" "profiles" ".*" "userChrome")
